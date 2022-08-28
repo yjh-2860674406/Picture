c4ebdda2535aa44227a157e40775a5a12f93ce04
叶锦华 <74096303+yjh-2860674406@users.noreply.github.com>
2022-08-28 23:55:20 +0800

    Update api.php

--- api.php
+++ api.php
@@ -4,7 +4,7 @@ $seed = time();
 //获取随机数
 $num = rand(1,80);
 //拼接图片地址
-$picpath = "https://cdn.jsdelivr.net/gh/dearxuan7392/picture/website/background/".$num.".jpg";
+$picpath = "https://github.com/yjh-2860674406/Picture/blob/main/%E7%89%9B%E6%B2%B9%E6%9E%9C.jpg";
 //重定位到图片
 die(header("Location: $picpath"));
 ?>
